e17d2a7328807a7c8094090cd432dc9ab36f4f28
maurofim <mauro.fim92@gmail.com>
2022-08-12 11:00:55 -0700

    Added the working dir change and some changes to the regex

--- metadataeraser.py
+++ metadataeraser.py
@@ -2,23 +2,24 @@
 #mat2 needed
 
 import re
-import subprocess
 import os
-
 #specify directory location
-path_directory = str(input('enter path directory: '))
+path = str(input('enter path directory: '))
+
+#changing working directory
+os.chdir(path)
 
 #create clean.txt and add all the metadata into it
 #then erase metadata
-for file in os.listdir(path_directory):
-    os.system('exiftool ' + str(file) + ' >>' + path_directory +' clean.txt')
+for file in os.listdir(path):
+    os.system('exiftool ' + file + ' >> clean.txt')
     os.system('echo -------------------------------------------- >> clean.txt')
-    os.system('mat2 ' + str(file))
+    os.system('mat2 ' + file)
 
-#create directory clean and move clened files into it
-os.mkdir(path_directory + '/clean')
-os.system('mv clean.txt' + path_directory + '/clean')
-end = re.search('cleaned\.\w\w\w$')
-for files in os.listdir(path_directory):
-    if files.endswith(end):
-        os.system('mv ' + str(files) + ' clean')
+#create directory clean and move cleaned files into it
+os.mkdir(path + '/clean')
+os.system('mv clean.txt ' + path + '/clean')
+end = '\.cleaned\..*$'
+for file in os.listdir(path):
+    if re.search(file, end):
+        os.system('mv ' + file + ' clean')
