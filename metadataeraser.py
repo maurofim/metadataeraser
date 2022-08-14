fc20de6206f8d2040b335247b03de565a9f251a4
maurofim <mauro.fim92@gmail.com>
2022-08-14 01:20:37 -0700

    solve issues with subprocess, extracting info from the generated tuple; echo corrections

--- metadataeraser.py
+++ metadataeraser.py
@@ -11,25 +11,25 @@ path = str(input('enter path directory: '))
 #changing working directory
 os.chdir(path)
 
-#create clean.txt and add all the metadata into it
+#create clean.txt and not_clean.txt, then add all the metadata into it
 #then erase metadata
 for file in os.listdir(path):
-    cleaning_command = subprocess.Popen(['mat2', '"', file, '"'], stdout = subprocess.PIPE)
-    output = str(cleaning_command.communicate())
-    if output == '':
-        os.system('CLEANED >> clean.txt')
+    cleaning_command = subprocess.Popen(['mat2', file], stdout = subprocess.PIPE)
+    output = cleaning_command.communicate()[0]
+    if output == b'':
+        os.system('echo CLEANED >> clean.txt')
         os.system('exiftool ' + '"' + file + '"' + ' >> clean.txt')
         os.system('echo -------------------------------------------- >> clean.txt')
     else:
-        os.system(output + ' >> not_clean.txt')
+        os.system('echo ' + str(output) + ' >> not_clean.txt')
         os.system('exiftool ' + '"' + file + '"' + ' >> not_clean.txt')
         os.system('echo -------------------------------------------- >> not_clean.txt')
 
 #create directories and move their respective files into them, .clean. >> /clean and .txt >> /clean_info
 os.mkdir(path + '/cleaned')
-os.mkdir(path + '/info')
-os.system('mv clean.txt' + ' ' + path + '/info')
-os.system('mv not_clean.txt' + ' ' + path + '/info')
+os.mkdir(path + '/cleaned_info')
+os.system('mv clean.txt' + ' ' + path + '/cleaned_info')
+os.system('mv not_clean.txt' + ' ' + path + '/cleaned_info')
 end = '^.*\.cleaned\..*$'
 for file in os.listdir(path):
     if re.match(end, file):
