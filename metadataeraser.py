a6a40ebdc9ab577e642f16c14fad6b9f4744d628
maurofim <mauro.fim92@gmail.com>
2022-08-18 12:42:29 -0700

    It works now yeeeeaaaaaaahh, exiftool and mat2 data is now collected with .communicate() and save in a variable that later on is put to a txt file, also created a cleaned_file() to make it easy to find the cleaned files.

--- metadataeraser.py
+++ metadataeraser.py
@@ -15,7 +15,7 @@ def get_parent_dir(file_path):
     return (parent_dir_path, current_file)
 
 #create cleaned directories
-def cleaned_dirs(path, dirs, root, subdirs):
+def cleaned_dirs(path, dirs, subdirs, root):
     r_path = '\/[\w ]*'
     for dir in dirs, subdirs:
         dir_path_end = root.replace(get_parent_dir(path)[0],'')
@@ -39,37 +39,43 @@ def path_cleaned_dir(origin_file, root, path):
     for name in dir_list:
         name = name + '.cleaned'
         cleaned_path +=  name
-    final_cleaned_path = parent_path + cleaned_path + '/' + origin_file
+    final_cleaned_path = get_parent_dir(path)[0] + cleaned_path + '/' + origin_file
     return final_cleaned_path
+
+def cleaned_file(file):
+    x = file.index('.')
+    name1 = file[:x]
+    name2 = file[x:]
+    cleaned_file = name1 + '.cleaned' + name2
+    return cleaned_file
 
 #creating cleaned files and txt files
 #moving cleaned files and txt files to their respective dir
-def cleaned_files(root, files, path, dirs):
+def cleaned_files(root, files, path):
     txt_path = get_parent_dir(path)[0]
     for file in files:
         os.chdir(root)
+        #gather metadata
+        cleaning_command = subprocess.Popen(['exiftool', file], stdout = subprocess.PIPE)
+        metadata = cleaning_command.communicate()[0]
         #erase metadata
         cleaning_command = subprocess.Popen(['mat2', file], stdout = subprocess.PIPE)
         output = cleaning_command.communicate()[0]
-        os.system('exiftool ' + '"' + file + '"' + ' >> cleaned_file.txt')
-        shutil.move('cleaned_file.txt', txt_path)
         os.chdir(txt_path)
         #check if the file is supported by mat2
         if output == b'':
             #create cleaned.txt add metadata
             os.system('echo CLEANED >> cleaned.txt')
             #adding "" for files with white spaces, this way linux terminal can read them
-            os.system('cleaned_file.txt >> cleaned.txt')
-            os.system('rm cleaned_file.txt')
+            os.system('echo ' + str(metadata) + ' >> cleaned.txt')
             os.system('echo -------------------------------------------- >> cleaned.txt')
             #move cleaned file to respective cleaned directory
-            shutil.move((root + '/' +file + '.cleaned'), path_cleaned_dir(file, root, path))
+            shutil.move((root + '/' +cleaned_file(file)), path_cleaned_dir(cleaned_file(file), root, path))
         else:
             #create not_cleaned.txt add metadata
             os.system('echo ' + str(output) + ' >> not_cleaned.txt')
-            os.system('echo location: ' + str(path_cleaned_dir(file, root, path)) + ' >> not_cleaned.txt')
-            os.system('cleaned_file.txt >> not_cleaned.txt')
-            os.system('rm cleaned_file.txt')
+            os.system('echo ' + str(metadata) + ' >> not_cleaned.txt')
+            os.system('echo location.cleaned: ' + str(path_cleaned_dir(file, root, path)) + ' >> not_cleaned.txt')
             os.system('echo -------------------------------------------- >> not_cleaned.txt')
             #move copy of file to respective cleaned directory
             shutil.copy((root + '/' + file), path_cleaned_dir(file, root, path))
@@ -78,8 +84,8 @@ def main():
     #specify directory location
     path = str(input('enter path directory: '))
     for root, dirs, subdirs in os.walk(path):
-        cleaned_dirs(path, dirs, root, subdirs)
+        cleaned_dirs(path, dirs, subdirs, root)
     for root, dirs, files in os.walk(path):
-        cleaned_files(path, files, root, dirs)
+            cleaned_files(root, files, path)
 
 main()
