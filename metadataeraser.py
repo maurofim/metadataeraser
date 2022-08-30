b387770b22f11d30d6648bdd07f371d746b47641
maurofim <mauro.fim92@gmail.com>
2022-08-30 09:39:39 -0700

    Debugged version

--- metadataeraser.py
+++ metadataeraser.py
@@ -1,58 +1,59 @@
 #!/usr/bin/env python3
 #exiftool needed
 #mat2 needed
+"""Cleans all metadata in a specified directory using mat2 and retrieves metadata with exiftool"""
 
 import re
 import os
 import shutil
 import subprocess
 
+#specify directory location
+PATH = str(input('enter path directory: '))
 #create path to the parent directory
-def get_parent_dir(file_path):
-    r_path = '\/[\w ]*$'
-    current_file = re.search(r_path, file_path).group()
-    parent_dir_path = file_path.replace(current_file,'')
-    return (parent_dir_path, current_file)
+class Data():
+    """Storing most used data to a class variable"""
+    path = PATH
+    current_dir = re.search('[/][A-za-z0-9 ]*$', path).group()
+    parent_dir = path.replace(current_dir,'')
 
-#create cleaned directories
-def cleaned_dirs(path, dirs, subdirs, root):
-    r_path = '\/[\w ]*'
-    for dir in dirs, subdirs:
-        dir_path_end = root.replace(get_parent_dir(path)[0],'')
-        dir_list = re.findall(r_path, dir_path_end)
-        cleaned_path = ''
-        for name in dir_list:
-            name = name + '.cleaned'
-            cleaned_path += name
-        if os.path.exists(get_parent_dir(path)[0] + cleaned_path):
-            pass
-        else:
-            os.mkdir(get_parent_dir(path)[0] + cleaned_path)
+def cleaned_dirs(root):
+    """create cleaned directories"""
+    r_path = '[/][A-za-z0-9 ]*'
+    dir_path_end = root.replace(Data.parent_dir, '')
+    dir_list = re.findall(r_path, dir_path_end)
+    cleaned_path = ''
+    for name in dir_list:
+        name = name + '.cleaned'
+        cleaned_path += name
+    if os.path.exists(Data.parent_dir + cleaned_path):
+        pass
+    else:
+        os.mkdir(Data.parent_dir + cleaned_path)
 
-#find cleaned dir for file.cleaned
-def path_cleaned_dir(origin_file, root, path):
-    r_path = '\/[\w ]*'
-    parent_path = get_parent_dir(path)[0]
-    final_path = root.replace(parent_path, '')
+def path_cleaned_dir(origin_file, root):
+    """find cleaned dir for file.cleaned"""
+    r_path = '[/][A-za-z0-9 ]*'
+    final_path = root.replace(Data.parent_dir, '')
     dir_list = re.findall(r_path, final_path)
     cleaned_path = ''
     for name in dir_list:
         name = name + '.cleaned'
         cleaned_path +=  name
-    final_cleaned_path = get_parent_dir(path)[0] + cleaned_path + '/' + origin_file
+    final_cleaned_path = Data.parent_dir + cleaned_path + '/' + origin_file
     return final_cleaned_path
 
 def cleaned_file(file):
-    x = file.index('.')
-    name1 = file[:x]
-    name2 = file[x:]
-    cleaned_file = name1 + '.cleaned' + name2
-    return cleaned_file
+    """find cleaned file name"""
+    dot = file.index('.')
+    name1 = file[:dot]
+    name2 = file[dot:]
+    cleaned = name1 + '.cleaned' + name2
+    return cleaned
 
-#creating cleaned files and txt files
-#moving cleaned files and txt files to their respective dir
-def cleaned_files(root, files, path):
-    txt_path = get_parent_dir(path)[0]
+def cleaned_files(root, files):
+    """creates and moves cleaned files to their respective dir;
+    also creates txts files and append info to them"""
     for file in files:
         os.chdir(root)
         #gather metadata
@@ -61,7 +62,7 @@ def cleaned_files(root, files, path):
         #erase metadata
         cleaning_command = subprocess.Popen(['mat2', file], stdout = subprocess.PIPE)
         output = cleaning_command.communicate()[0]
-        os.chdir(txt_path)
+        os.chdir(Data.parent_dir)
         #check if the file is supported by mat2
         if output == b'':
             #create cleaned.txt add metadata
@@ -70,22 +71,22 @@ def cleaned_files(root, files, path):
             os.system('echo ' + str(metadata) + ' >> cleaned.txt')
             os.system('echo -------------------------------------------- >> cleaned.txt')
             #move cleaned file to respective cleaned directory
-            shutil.move((root + '/' +cleaned_file(file)), path_cleaned_dir(cleaned_file(file), root, path))
+            shutil.move((root + '/' + cleaned_file(file)),
+                        path_cleaned_dir(cleaned_file(file), root))
         else:
             #create not_cleaned.txt add metadata
             os.system('echo ' + str(output) + ' >> not_cleaned.txt')
             os.system('echo ' + str(metadata) + ' >> not_cleaned.txt')
-            os.system('echo location.cleaned: ' + str(path_cleaned_dir(file, root, path)) + ' >> not_cleaned.txt')
+            os.system('echo location.cleaned: ' + str(path_cleaned_dir(file, root))
+                      + ' >> not_cleaned.txt')
             os.system('echo -------------------------------------------- >> not_cleaned.txt')
             #move copy of file to respective cleaned directory
-            shutil.copy((root + '/' + file), path_cleaned_dir(file, root, path))
-
+            shutil.copy((root + '/' + file), path_cleaned_dir(file, root))
 def main():
-    #specify directory location
-    path = str(input('enter path directory: '))
-    for root, dirs, subdirs in os.walk(path):
-        cleaned_dirs(path, dirs, subdirs, root)
-    for root, dirs, files in os.walk(path):
-            cleaned_files(root, files, path)
+    """master function"""
+    for root in os.walk(Data.path):
+        cleaned_dirs(root[0])
+    for root in os.walk(Data.path):
+        cleaned_files(root[0], root[2])
 
 main()
